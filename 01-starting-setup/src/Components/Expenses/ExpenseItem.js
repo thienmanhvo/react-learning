913a26596706876ff65a1cd1ad1783c999149bf4
thienmanhvo <benficacronaldo@gmail.com>
2021-12-05 16:07:55 +0700

    assignment 2

--- 01-starting-setup/src/Components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/Components/Expenses/ExpenseItem.js
@@ -5,7 +5,6 @@ import { useState } from "react";
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
-  console.log(title);
   const clickHandler = () => {
     setTitle("Updated!!!");
   };
--- 01-starting-setup/src/Components/Expenses/Expenses.js
+++ 01-starting-setup/src/Components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selectedYear={filteredYear}
+        onDropdownSelected={filterChangeHandler}
+      />
       {props.expenses.map((element) => (
         <ExpenseItem
           title={element.title}
