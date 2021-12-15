b95c744b72874ede53333ae04b630fc18c1c3ad1
thienmanhvo <benficacronaldo@gmail.com>
2021-12-15 23:37:46 +0700

    useEffect bump

--- 11-starting-project/src/components/Layout/HeaderCartButton.js
+++ 11-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,12 +1,31 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { useState } from "react/cjs/react.development";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   var cartContext = useContext(CartContext);
+  const [isButtonHighlighted, setIsButtonHighlighted] = useState(false);
+  const buttonClass = `${classes.button} ${
+    isButtonHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) return;
+    setIsButtonHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setIsButtonHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
+
   return (
-    <button className={classes.button} onClick={props.onShowCart}>
+    <button className={buttonClass} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
