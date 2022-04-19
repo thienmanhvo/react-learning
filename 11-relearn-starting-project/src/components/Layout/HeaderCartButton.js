4eaffac901af4ee46c7f4d06d08f71cbb4d0cad0
thienmanhvo <benficacronaldo@gmail.com>
2022-04-19 18:34:09 +0700

    cart reducer

--- 11-relearn-starting-project/src/components/Layout/HeaderCartButton.js
+++ 11-relearn-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>
+        {cartCtx.item.reduce((total, current) => {
+          return total + current;
+        }, 0)}
+      </span>
     </button>
   );
 };
--- 11-relearn-starting-project/src/store/CartProvider.js
+++ 11-relearn-starting-project/src/store/CartProvider.js
@@ -1,12 +1,59 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+  item: [],
+  totalAmount: 0
+};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updeatedItem = [...state.item];
+    const index = state.item.findIndex((item) => item.id === action.item.id);
+    if (index === -1) {
+      updeatedItem.push(action.item);
+    } else {
+      updeatedItem[index].amount += action.item.amount;
+    }
+    const totalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return { items: updeatedItem, totalAmount: totalAmount };
+  }
+  if (action.type === "REMOVE") {
+    const index = state.items.findIndex((item) => item.id === action.id);
+    let removedList = [...state.items];
+    const removedItem = removedList[index];
+
+    if (removedItem.amount > 1) {
+      removedList[index].amount--;
+    } else {
+      removedList.splice(index, 1);
+    }
+    const totalAmount = state.totalAmount - removedItem.price;
+    return {
+      items: removedList,
+      totalAmount: totalAmount
+    };
+  }
+
+  return defaultCartState;
+};
+
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    item: [],
-    totalAmount: 0,
+    item: cartState.item,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler
   };
